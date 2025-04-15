ee811e10adc53db027bf5255d6c519b82a2b199e
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-04-14 21:46:37 -0600

    Traducir texto en el componente Pricing para mejorar la claridad del mensaje

--- components/Pricing.js
+++ components/Pricing.js
@@ -28,8 +28,8 @@ const Pricing = () => {
 							variants={scrollAnimation}
 							className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
 						>
-							Let's choose the package that is best for you and explore it happily
-							and cheerfully.
+							Elige el paquete que se adapta a tus necesidades y da el siguiente paso.
+
 						</motion.p>
 					</ScrollAnimationWrapper>
 					<div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
