69b2c72e3c3314ce9568536cddeee840c4096c6b
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-09-19 21:50:24 -0600

    Eliminar importación de LogoVPN y sustituir por URL externa en el componente Header

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
 import ButtonOutline from "../misc/ButtonOutline.";
-import LogoVPN from "../../public/assets/Logo.svg";
+// Se eliminó la importación del LogoVPN ya que usaremos una URL externa
 
 const Header = () => {
   const [activeLink, setActiveLink] = useState(null);
@@ -30,7 +30,12 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
-            <LogoVPN className="h-20 w-auto" />
+            {/* --- AQUÍ SE SUSTITUYÓ LA IMAGEN --- */}
+            <img 
+              src="https://directorio.paqueteriaestrellablanca.com/wp-content/webp-express/webp-images/uploads/2022/10/AF1QipNd0k0VyqGvYJa-cWzPyQ3IA8-7McqA1ngvYq_Uw408-h468-k-no.png.webp" 
+              className="h-20 w-auto" 
+              alt="Logo Paquetería Estrella Blanca"
+            />
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
