b9bb523e4905c39ad7a436982e84372dbb46a0d7
mariorz665 <mariorz665@outlook.com>
2025-09-23 15:25:09 -0600

    Actualizar componente Testimoni: reemplazar testimonios de usuarios y ajustar el formato para mejorar la presentación.

--- components/Testimoni.js
+++ components/Testimoni.js
@@ -10,134 +10,143 @@ import ArrowNext from "../public/assets/Icon/eva_arrow-next-fill.svg";
 const Testimoni = ({
   listTestimoni = [
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "Mariana Solís",
+      image: "/assets/people-1.png",
+      city: "Tuxtepec",
+      country: "Oaxaca",
+      rating: "4.9",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "Como dueña de un negocio de e-commerce, la puntualidad es clave. Con Rapidos de la Cuenca, mis productos siempre llegan a tiempo a mis clientes. Se han convertido en un socio comercial indispensable para mí.",
     },
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "Jorge Espinosa",
+      image: "/assets/people-2.png",
+      city: "Cosamaloapan",
+      country: "Veracruz",
+      rating: "5.0",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "Envié una pieza de alfarería muy delicada a mi hermana y llegó intacta. El servicio de empaque y el cuidado que le pusieron fue excepcional. Ofrecen una tranquilidad que no tiene precio. ¡Totalmente de confianza!",
     },
     {
-      name: "iezh Robert",
+      name: "Daniela Cruz",
       image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      city: "Loma Bonita",
+      country: "Oaxaca",
+      rating: "4.8",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "Necesitaba recibir unos documentos urgentes para un trámite y su servicio exprés fue increíblemente rápido y eficiente. El sistema de seguimiento en línea es muy preciso y fácil de usar. Me salvaron de un apuro.",
     },
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
+      name: "Roberto Fuentes",
+      image: "/assets/people-1.png",
+      city: "Tierra Blanca",
+      country: "Veracruz",
+      rating: "5.0",
       testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+        "Lo que más valoro es la amabilidad del personal. Tanto en la sucursal como los repartidores, siempre te atienden con una sonrisa. Se siente como un servicio local y cercano, a pesar de su gran profesionalismo.",
     },
-  ],
+    {
+        name: "Laura Vicencio",
+        image: "/assets/people-2.png",
+        city: "Puebla",
+        country: "Puebla",
+        rating: "4.7",
+        testimoni:
+          "Mi familia me envía productos de la región y siempre usan esta paquetería. A pesar de la distancia, todo llega fresco y en buen estado. Es genial tener una opción tan confiable que conecte nuestra región con otras ciudades.",
+      }
+],
 }) => {
   const settings = {
-    dots: true,
-    customPaging: function (i) {
-      return (
-        <a className="">
-          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
-        </a>
-      );
-    },
-    dotsClass: "slick-dots w-max absolute mt-20  ",
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 770,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
+	 dots: true,
+	 customPaging: function (i) {
+		return (
+		  <a className="">
+			 <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+		  </a>
+		);
+	 },
+	 dotsClass: "slick-dots w-max absolute mt-20  ",
+	 infinite: true,
+	 speed: 500,
+	 slidesToShow: 3,
+	 slidesToScroll: 2,
+	 responsive: [
+		{
+		  breakpoint: 770,
+		  settings: {
+			 slidesToShow: 2,
+			 slidesToScroll: 2,
+			 initialSlide: 2,
+		  },
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			 slidesToShow: 1,
+			 slidesToScroll: 1,
+		  },
+		},
+	 ],
   };
   const [sliderRef, setSliderRef] = useState(null);
 
   return (
-    <>
-      <Slider
-        {...settings}
-        arrows={false}
-        ref={setSliderRef}
-        className="flex items-stretch justify-items-stretch"
-      >
-        {listTestimoni.map((listTestimonis, index) => (
-          <div className="px-3 flex items-stretch" key={index}>
-            <div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
-              <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
-                <div className="flex order-2 xl:order-1">
-                  <Image
-                    src={listTestimonis.image}
-                    height={50}
-                    width={50}
-                    alt="Icon People"
-                  />
-                  <div className="flex flex-col ml-5 text-left">
-                    <p className="text-lg text-black-600 capitalize">
-                      {listTestimonis.name}
-                    </p>
-                    <p className="text-sm text-black-500 capitalize">
-                      {listTestimonis.city},{listTestimonis.country}
-                    </p>
-                  </div>
-                </div>
-                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
-                  <p className="text-sm">{listTestimonis.rating}</p>
-                  <span className="flex ml-4">
-                    <Stars className="h-4 w-4" />
-                  </span>
-                </div>
-              </div>
-              <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
-            </div>
-          </div>
-        ))}
-      </Slider>
-      <div className="flex w-full items-center justify-end">
-        <div className="flex flex-none justify-between w-auto mt-14">
-          <div
-            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
-            onClick={sliderRef?.slickPrev}
-          >
-            <ArrowBack className="h-6 w-6 " />
-          </div>
-          <div
-            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
-            onClick={sliderRef?.slickNext}
-          >
-            <ArrowNext className="h-6 w-6" />
-          </div>
-        </div>
-      </div>
-    </>
+	 <>
+		<Slider
+		  {...settings}
+		  arrows={false}
+		  ref={setSliderRef}
+		  className="flex items-stretch justify-items-stretch"
+		>
+		  {listTestimoni.map((listTestimonis, index) => (
+			 <div className="px-3 flex items-stretch" key={index}>
+				<div className="border-2 border-gray-500 hover:border-orange-500 transition-all rounded-lg p-8 flex flex-col">
+				  <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
+					 <div className="flex order-2 xl:order-1">
+						<Image
+						  src={listTestimonis.image}
+						  height={50}
+						  width={50}
+						  alt="Icon People"
+						/>
+						<div className="flex flex-col ml-5 text-left">
+						  <p className="text-lg text-black-600 capitalize">
+							 {listTestimonis.name}
+						  </p>
+						  <p className="text-sm text-black-500 capitalize">
+							 {listTestimonis.city},{listTestimonis.country}
+						  </p>
+						</div>
+					 </div>
+					 <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
+						<p className="text-sm">{listTestimonis.rating}</p>
+						<span className="flex ml-4">
+						  <Stars className="h-4 w-4" />
+						</span>
+					 </div>
+				  </div>
+				  <p className="mt-5 text-left">“{listTestimonis.testimoni}”.</p>
+				</div>
+			 </div>
+		  ))}
+		</Slider>
+		<div className="flex w-full items-center justify-end">
+		  <div className="flex flex-none justify-between w-auto mt-14">
+			 <div
+				className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+				onClick={sliderRef?.slickPrev}
+			 >
+				<ArrowBack className="h-6 w-6 " />
+			 </div>
+			 <div
+				className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
+				onClick={sliderRef?.slickNext}
+			 >
+				<ArrowNext className="h-6 w-6" />
+			 </div>
+		  </div>
+		</div>
+	 </>
   );
 };
 
