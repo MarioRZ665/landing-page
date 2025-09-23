362d7e74b20c56a2e2392c8d7d07f3937c38af3e
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-09-22 20:47:38 -0600

    Actualizar componente Pricing: ajustar márgenes en secciones y modificar clases para mejorar el diseño responsivo.

--- components/Pricing.js
+++ components/Pricing.js
@@ -82,7 +82,7 @@ const Pricing = () => {
 				</div>
 
 				{/* --- Sección de Red de Cobertura --- */}
-				<div className="flex flex-col w-full my-16">
+				<div className="flex flex-col w-full my-14">
 					<ScrollAnimationWrapper>
 						<motion.h3
 							variants={scrollAnimation}
@@ -184,11 +184,11 @@ const Pricing = () => {
 				</div>
 
 				{/* --- Sección de Testimonios --- */}
-				<div className="flex flex-col w-full my-16" id="testimoni">
+				<div className="flex flex-col w-full my-13" id="testimoni">
 					<ScrollAnimationWrapper>
 						<motion.h3
 							variants={scrollAnimation}
-							className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
+							className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/13 sm: lg:w-4/13 mx-auto">
 							La Confianza de Cientos de Clientes Satisfechos
 						</motion.h3>
 						<motion.p
@@ -201,27 +201,6 @@ const Pricing = () => {
 					<ScrollAnimationWrapper className="w-full flex flex-col py-12">
 						<motion.div variants={scrollAnimation}>
 							<Testimoni />
-						</motion.div>
-					</ScrollAnimationWrapper>
-				</div>
-
-				{/* --- Sección de CTA Final --- */}
-				<div className="relative w-full mt-16">
-					<ScrollAnimationWrapper>
-						<motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
-							<div className="absolute rounded-xl py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
-								<div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
-									<h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
-										Optimiza tus Envíos Hoy Mismo: <br /> Cotiza tu Servicio de Paquetería
-									</h5>
-									<p>¿Buscas   optimizar la logística de tu negocio  ? Hablemos de cómo podemos ayudarte a   llegar más lejos  .</p>
-								</div>
-								<ButtonPrimary>Empezar Ahora</ButtonPrimary>
-							</div>
-							<div
-								className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
-								style={{ filter: "blur(114px)" }}
-							></div>
 						</motion.div>
 					</ScrollAnimationWrapper>
 				</div>
