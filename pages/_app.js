79fec0f5906068bec0a3c655b3b28d8fd2e85017
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-09-23 15:51:06 -0600

    Agregar Google Analytics GA4 al componente MyApp

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,30 @@
 import "../styles/tailwind.css";
 import "../styles/slick.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Google Analytics GA4 */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-99KNBWD8PP"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-99KNBWD8PP');
+            `,
+          }}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
