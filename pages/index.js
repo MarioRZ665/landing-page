60b8caf2af64e2f679f33f2e17369b411d8144f2
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-09-23 19:04:08 -0600

    update

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,51 @@ export default function Home() {
 		<>
 			<SeoHead
 				title="Rápidos de la Cuenca | Paquetería y Mensajería Segura en México"
-				description="Envíos rápidos y confiables con Rápidos de la Cuenca. Rastrea tu paquete en línea, mensajería foránea y paquetería segura en todo México."
-				keywords="paquetería, mensajería, envíos, logística, rastreo de paquetes, Rápidos de la Cuenca"
+				description="Envíos rápidos, seguros y confiables con Rápidos de la Cuenca. Rastrea tu paquete en línea, servicio de mensajería foránea y paquetería nacional en todo México."
+				keywords="paquetería en México, mensajería segura, envíos nacionales, rastreo de paquetes, logística México, Rápidos de la Cuenca"
+				canonical="https://www.rapidosdelacuenca.com/"
+				ogTitle="Rápidos de la Cuenca | Paquetería y Mensajería Segura en México"
+				ogDescription="Tu servicio de paquetería y mensajería confiable en México. Rápidos de la Cuenca: rapidez, seguridad y confianza en cada envío."
+				ogImage="https://www.rapidosdelacuenca.com/images/og-image.jpg"
+				ogUrl="https://www.rapidosdelacuenca.com/"
+				twitterCard="summary_large_image"
+				twitterSite="@rapidosdelacuenca"
 			/>
 			<Layout>
 				<Hero />
 				<Feature />
 				<Pricing />
+
+				{/* Agregamos JSON-LD para SEO estructurado */}
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify({
+							"@context": "https://schema.org",
+							"@type": "ParcelService",
+							name: "Rápidos de la Cuenca",
+							url: "https://www.rapidosdelacuenca.com",
+							logo: "https://www.rapidosdelacuenca.com/images/logo.png",
+							description:
+								"Servicio de paquetería y mensajería segura en México. Rastreo en línea, envíos foráneos y logística confiable.",
+							areaServed: {
+								"@type": "Country",
+								name: "México",
+							},
+							sameAs: [
+								"https://www.facebook.com/rapidosdelacuenca",
+								"https://twitter.com/rapidosdelacuenca",
+								"https://www.instagram.com/rapidosdelacuenca",
+							],
+							contactPoint: {
+								"@type": "ContactPoint",
+								telephone: "+52-2214079186",
+								contactType: "customer service",
+								availableLanguage: ["Spanish", "English"],
+							},
+						}),
+					}}
+				/>
 			</Layout>
 		</>
 	);
