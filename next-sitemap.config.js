e52194a9c98dc83144c326663c8b08272e42acb4
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-09-30 22:23:17 -0600

    Actualizar componente Hero: renombrar propiedad de estadísticas, mejorar estructura y estilos, y ajustar colores de íconos y botones.

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://rapidosdelacuenca.com.mx',
+  generateRobotsTxt: true, 
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+};
