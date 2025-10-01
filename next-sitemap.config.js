2c8939602d088af0be2172c4d31091dfb0c20330
Mario RZ <49970661+MarioRZ665@users.noreply.github.com>
2025-10-01 17:15:38 -0600

    Remove unused assets and update sitemap configuration
    - Deleted various PNG files from the public/assets directory, including customer reviews, web development images, and people images.
    - Removed favicon images and related files from the public/favicon directory.
    - Deleted the site.webmanifest file and browserconfig.xml.
    - Added a new robots.txt file to allow all user agents and specify the sitemap location.
    - Created a new sitemap-0.xml file and updated sitemap.xml to reference it.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,19 @@
-// next-sitemap.config.js
-
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://rapidosdelacuenca.com.mx',
-  generateRobotsTxt: true, 
+  generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'daily',
   priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [
+      'https://rapidosdelacuenca.com.mx/sitemap.xml',
+    ],
+  },
 };
